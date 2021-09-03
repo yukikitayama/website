f454a95ebab23ebce8ff831e44870c0fdf76484c
Yuki Kitayama <yk2797@columbia.edu>
2021-09-02 22:09:31 -0600

    deploy a new version with table and single post page

--- src/app/posts/post-table/post-table.component.ts
+++ src/app/posts/post-table/post-table.component.ts
@@ -3,24 +3,10 @@ import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
+// import { MatSort } from '@angular/material/sort';
 
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
-
-const TITLES: string[] = [
-  'Title1', 'Title2'
-];
-const CATEGORIES: string[] = [
-  'AWS', 'Google Cloud'
-]
-const DATES: string[] = [
-  '2021-07-01', '2021-07-02'
-]
-
-const IDS: string[] = [
-  '61245b008c67a201c82f0ba7', '6107102eb42867b116feb322'
-]
 
 @Component({
   selector: 'app-post-table',
@@ -39,22 +25,9 @@ export class PostTableComponent implements OnInit, AfterViewInit, OnDestroy {
   pageSizeOptions: number[] = [10, 20, 50];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
+  // @ViewChild(MatSort) sort: MatSort;
 
-  constructor(
-    private router: Router,
-    private postsService: PostsService
-  ) {
-    // Create array of Post objects
-    // const posts = Array.from([0, 1], x=> createNewPost(x));
-    // Assign the data to the data source for the table to render
-    // this.dataSource = new MatTableDataSource(posts);
-    // this.postsService
-    //   .getAllPosts()
-    //   .subscribe((response) => {
-    //     this.dataSource = new MatTableDataSource(response.posts);
-    //   });
-  }
+  constructor(private router: Router, private postsService: PostsService) {}
 
   ngOnInit(): void {
     this.isLoading = true;
@@ -96,15 +69,4 @@ export class PostTableComponent implements OnInit, AfterViewInit, OnDestroy {
   ngOnDestroy() {
     this.postsSub.unsubscribe();
   }
-}
-
-// Builds and returns a new Post.
-function createNewPost(id: number): Post {
-  return {
-    id: IDS[id],
-    title: TITLES[id],
-    category: CATEGORIES[id],
-    date: DATES[id],
-    content: 'content' + id.toString()
-  };
 }
